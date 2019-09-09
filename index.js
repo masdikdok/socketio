934d88b5d1d39c06767dd2bbacebae0d3c6da671
Dicki Andrea <dicki123andrea@gmail.com>
2019-09-09 11:29:58 +0700

    update add chat group function

---
+++ index.js
@@ -0,0 +1,114 @@
+let express = require('express');
+let app = express();
+let server = require('http').createServer(app);
+let io = require('socket.io')(server);
+
+app.use(express.static(__dirname + '/public'));
+
+server.listen(3000, function() {
+    console.log('listening on *:3000');
+});
+
+
+
+let jumlahUser = 0;
+let kumpulanUser = {};
+let userMengetik = [];
+
+function cekSedangMengetik(v){
+	if(userMengetik.indexOf(v) >= 0){
+		return userMengetik;
+	}else{
+		userMengetik.push(v);
+	}
+	return userMengetik;
+}
+
+function hapusArray(array, v){
+	return array.filter(item => item != v);
+}
+
+function cekUser(v){
+	let val = '';
+	for(let key in kumpulanUser){
+		if(key === v){
+			val = key;
+		}
+	}
+	return val;
+}
+
+
+var test = io
+    .of('/test')
+    .on('connection', function(socket) {
+        socket.emit('news', {
+            hello: 'world'
+        });
+        socket.on('my other event', function(data) {
+            console.log(data);
+        });
+    });
+
+var chat = io
+    .of('/chat')
+    .on('connection', (socket) => {
+
+        socket.on('userConnect', (data) => {
+            if (cekUser(data) === '') {
+                socket.username = data;
+                kumpulanUser[data] = socket.id;
+                jumlahUser++;
+                socket.emit('setUser', {
+                    'userName': data,
+                    'userID': socket.id
+                });
+
+                io.emit('updateUser', {
+                    'jumlahUser': jumlahUser,
+                    'kumpulanUser': kumpulanUser
+                });
+
+            } else {
+                socket.emit('userExist', 'userName "' + data + '" sudah ada sebelumnya !!');
+            }
+        });
+
+        socket.on('reloadData', (data) => {
+            if (cekUser(data.userNameSocket) === '') {
+                kumpulanUser[data.userNameSocket] = data.userIDSocket;
+                jumlahUser++;
+            }
+            io.emit('updateUser', {
+                'jumlahUser': jumlahUser,
+                'kumpulanUser': kumpulanUser
+            });
+
+        });
+
+        socket.on('userLogout', (data) => {
+            delete kumpulanUser[data.userName];
+            if (jumlahUser != 0) {
+                jumlahUser--;
+            }
+            io.emit('updateUser', {
+                'jumlahUser': jumlahUser,
+                'kumpulanUser': kumpulanUser
+            });
+
+            io.emit('laporanLogout', true);
+            io.emit('cekChatPrivate', data.userName);
+
+        });
+
+        socket.on('grupKeypress', (data) => {
+            socket.broadcast.emit('ongrupKeypress', cekSedangMengetik(data));
+        });
+
+        socket.on('kirimPesanGrup', (data) => {
+            userMengetik = hapusArray(userMengetik, data.pengirim);
+            socket.broadcast.emit('ongrupKeypress', userMengetik);
+            io.emit('pesanGrup', data);
+        })
+
+    });
---
+++ public/js/chat.js
@@ -0,0 +1,131 @@
+// Membuat koneksi
+let socket = io.connect('http://localhost:3000/chat');
+
+let myUsername = '';
+let userMengetik = [];
+
+function loggin(){
+	if (typeof(Storage) !== "undefined") {
+		if(localStorage.userIDSocket == null && localStorage.userNameSocket == null){
+			let pertama = prompt("Siapa nama Anda? ");
+			if(pertama != null){
+				socket.emit('userConnect', pertama);
+			}else{
+				window.open("about:blank",'_self').close();
+			}
+		}else{
+			alert("Selamat datang kembali "+ localStorage.userNameSocket + "\nHave a nice day :D !!");
+			myUsername = localStorage.userNameSocket;
+			socket.emit('reloadData', {
+				'userNameSocket' : localStorage.userNameSocket,
+				'userIDSocket' : localStorage.userIDSocket
+			});
+			$("span.myusername").html(myUsername);
+			$("span.myusername").html(myUsername);
+			$("#userOnline li:first").html(myUsername);
+		}
+	} else {
+		alert("Browser anda tidak mendukung untuk menyimpan data pribadi anda. \nKemungkinan data pribadi akan terus hilang.\nSilahkan lanjutkan..");
+	}
+}
+
+function logout(){
+	if(confirm("Apakah anda yakin ingin keluar dari aplikasi chat yang kece ini?")){
+		socket.emit('userLogout', {
+			'userName' : localStorage.userNameSocket,
+			'userID' : localStorage.userIDSocket
+		});
+		localStorage.removeItem
+	}
+}
+
+$(function(){
+	socket.on('connect', () => {
+		loggin();
+	});
+
+	socket.on('laporanLogout', (data) =>{
+		if(data == true){
+			localStorage.removeItem('userNameSocket');
+			localStorage.removeItem('userIDSocket');
+			alert("Logout berhasil!!");
+			window.open("about:blank",'_self').close();
+		}
+	});
+
+	socket.on('setUser', (data) => {
+		localStorage.setItem('userIDSocket', data.userID);
+		localStorage.setItem('userNameSocket', data.userName);
+		$("span.myusername").html(data.userName);
+		$("#userOnline li:first").html(data.userName);
+		myUsername = data.userName;
+	})
+
+	socket.on('updateUser', (data) =>{
+		$("#jumlahOnline").html(data.jumlahUser);
+		$('ul#userOnline li').not('li:first').remove();
+		$.each(data.kumpulanUser, function(key, value) {
+			if(key !== myUsername) {
+				$("#userOnline").append('<li class="list-group-item btn" onclick="startChat(\'' + key +'\')">' + key + '</li>');
+			}
+		});
+	});
+
+	socket.on('userExist', (data) => {
+		if(confirm(data + "\nMau coba lagi dengan nama lain?")){
+			loggin();
+		}else{
+			window.open("about:blank",'_self').close();
+		}
+	});
+
+	socket.on('cekChatPrivate', (data) => {
+		if($('span#penerima').text() == data){
+			$('span#penerima').html('None');
+			$('#isiChatPrivate').attr("disabled", "disabled");
+			$('#btnChatPrivate').attr("disabled", "disabled");
+		}
+	});
+
+	// Menampilkan keterangan "sedang mengetik" ketika user sedang mengetik
+	$('#isiChatGrup').on('keypress', () => {
+		socket.emit('grupKeypress', myUsername);
+	});
+
+	socket.on('ongrupKeypress', (data) => {
+		userMengetik = data.filter(item => item != myUsername);
+		if(userMengetik.length == 0){
+			$('#feedbackChatGrup').html('');
+		}else{
+			$('#feedbackChatGrup').html('<p><em>'+ userMengetik.join(', ') +' sedang mengetik..</em></p>');
+		}
+	});
+
+	$('#btnChatGrup').on('click', () => {
+		if($('#isiChatGrup').val() == ''){
+			alert("Pesan tidak boleh kosong!");
+		}else{
+			socket.emit('kirimPesanGrup', {
+				'pengirim' : myUsername,
+				'pesan': $('#isiChatGrup').val()
+			});
+			$('#isiChatGrup').val('');
+		}
+	});
+
+	socket.on('pesanGrup', (data) => {
+		if(data.pengirim == myUsername){
+			$('#outputChatGrup').append('<p style="text-align: right;"> '+ data.pesan + ' : <b>' + data.pengirim + ' </b></p>');
+		}else{
+			$('#outputChatGrup').append('<p><b>' + data.pengirim + ' </b> : '+ data.pesan + '</p>');
+		}
+	});
+});
+
+function startChat(key){
+	if(confirm("Apakah anda ingin memulai chat dengan " + key + " ?")){
+		$('span#penerima').html(key);
+		$('#isiChatPrivate').removeAttr("disabled");
+		$('#btnChatPrivate').removeAttr("disabled");
+	}
+}
