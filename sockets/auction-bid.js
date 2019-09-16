75f7a9d4790317f69757ce9f4683467775d68575
masdikdok <dicki123andrea@gmail.com>
2019-09-16 17:03:21 +0700

    update socket auctionBid

--- sockets/auction-bid.js
+++ sockets/auction-bid.js
@@ -1,14 +1,43 @@
 module.exports.listen = function(io) {
     const request = require('request');
     let auctionBid = io.of('/auction-bid');
+    let urlApi = '';
 
     auctionBid.on('connection', (socket) => {
+        socket.on('setUrlApi', (data) => {
+            urlApi = data;
+        });
 
         socket.on('updateDataFaktual', (data) => {
             socket.broadcast.emit('responseUpdateDataFaktual', data);
         });
 
+        setInterval(function(){
+            var options = {
+                method: 'POST',
+                url: urlApi + "/checkAuctionExpired",
+                headers: {
+                    'cache-control': 'no-cache',
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            };
+
+            request(options, function(error, response, body) {
+                if (error) throw new Error(error);
+
+                var hasilRequest = body;
+
+                if(hasilRequest.result){
+                    auctionBid.emit('checkAuctionExpired', hasilRequest.data);
+                }
+
+            });
+
+        }, 900000);
+
     });
+
 
     return io;
 }
