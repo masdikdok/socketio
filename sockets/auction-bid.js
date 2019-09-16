c3dfe8eb7217b742d3b0ef6e676fa4a505ff8231
masdikdok <dicki123andrea@gmail.com>
2019-09-16 23:38:20 +0700

    update add fungsi push notifikasi

--- sockets/auction-bid.js
+++ sockets/auction-bid.js
@@ -1,5 +1,6 @@
 module.exports.listen = function(io) {
     const request = require('request');
+
     let auctionBid = io.of('/auction-bid');
     let urlApi = '';
 
@@ -12,32 +13,30 @@ module.exports.listen = function(io) {
             socket.broadcast.emit('responseUpdateDataFaktual', data);
         });
 
-        setInterval(function(){
-            var options = {
-                method: 'POST',
-                url: urlApi + "/checkAuctionExpired",
-                headers: {
-                    'cache-control': 'no-cache',
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/x-www-form-urlencoded'
-                }
-            };
-
-            request(options, function(error, response, body) {
-                if (error) throw new Error(error);
-
-                var hasilRequest = body;
-
-                if(hasilRequest.result){
-                    auctionBid.emit('checkAuctionExpired', hasilRequest.data);
-                }
-
-            });
-
-        }, 900000);
-
     });
 
+    setInterval(function(){
+        var options = {
+            method: 'POST',
+            url: urlApi + "/checkAuctionExpired",
+            headers: {
+                'cache-control': 'no-cache',
+                'Accept': 'application/json',
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        };
+
+        request(options, function(error, response, body) {
+            if (error) throw new Error(error);
+
+            var hasilRequest = JSON.parse(body);
+            if(hasilRequest.result){
+                auctionBid.emit('checkAuctionExpired', hasilRequest.data);
+            }
+
+        });
+
+    }, 60000);
 
     return io;
 }
